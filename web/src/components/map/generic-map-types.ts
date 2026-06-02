9474e91e7dee37cfa87c6d0ceb4786b879137596
LovelyCat <pjrbuyvi@gmail.com>
2026-06-03 00:11:05 +0800

    feat(web): baidu map supports

--- web/src/components/map/generic-map-types.ts
+++ web/src/components/map/generic-map-types.ts
@@ -2,11 +2,11 @@ import type {BaiduMapConfig} from "@/components/map/baidu/BaiduMap.tsx";
 import type {HTMLProps} from "react";
 
 export enum GenericMapType {
-    BAIDU,
-    AMAP
+    BAIDU = "BAIDU",
+    AMAP = "AMAP",
 }
 
-export interface MapComponents {
+export interface GenericMapComponents {
     zoom?: boolean;
     fullScreen?: boolean;
     geoLocate?: boolean;
@@ -17,12 +17,55 @@ export interface MapComponents {
     scale?: boolean;
 }
 
+export interface GenericMapInitialProps {
+    center?: GenericLatLngPoint;
+    heading?: number;
+    pitch?: number;
+    range?: number;
+}
+
+export interface GenericMapEvent {
+    latLng: GenericLatLngPoint;
+}
+
+export interface GenericMapEvents {
+    onClick?: (e: GenericMapEvent) => void;
+    onMouseEnter?: (e: GenericMapEvent) => void;
+    onMouseLeave?: (e: GenericMapEvent) => void;
+    onMouseDown?: (e: GenericMapEvent) => void;
+    onMouseUp?: (e: GenericMapEvent) => void;
+}
+
+export type GenericMapCommonProps = {
+    initialProps?: GenericMapInitialProps;
+    components?: GenericMapComponents;
+    events?: GenericMapEvents;
+};
+
+
 export type GenericMapProps = HTMLProps<HTMLDivElement> & ({
     mapType: GenericMapType.BAIDU;
     mapSettings: BaiduMapConfig;
-    components: MapComponents;
 } | {
     mapType: GenericMapType.AMAP;
     settings: unknown;
-    components: MapComponents;
-});
+}) & GenericMapCommonProps;
+
+export type GenericLatLngPoint = [number, number];
+
+export interface GenericMapRef {
+    moveCameraTo: (
+        target: GenericLatLngPoint,
+        options?: {
+            duration?: number;
+            heading?: number;
+            pitch?: number;
+            range?: number;
+            onComplete?: () => void;
+        }
+    ) => void;
+    tools: {
+        addressToLatLngPoint: (address: string) => Promise<GenericLatLngPoint | null>;
+        latLngPointToAddress: (point: GenericLatLngPoint) => Promise<string | null>;
+    }
+}
