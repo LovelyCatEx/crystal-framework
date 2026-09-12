b32a5852b5307b638e25670088f35fda37d43bd9
LovelyCat <pjrbuyvi@gmail.com>
2026-09-13 01:05:21 +0800

    release: v1.15.0

--- crystal-shared/src/main/kotlin/com/lovelycatv/crystalframework/shared/constants/GlobalConstants.kt
+++ crystal-shared/src/main/kotlin/com/lovelycatv/crystalframework/shared/constants/GlobalConstants.kt
@@ -3,7 +3,7 @@ package com.lovelycatv.crystalframework.shared.constants
 import org.springframework.core.Ordered
 
 object GlobalConstants {
-    const val APP_VERSION = "1.14.3"
+    const val APP_VERSION = "1.15.0"
 
     const val REQUEST_MAPPING_PREFIX = "/api/{version}"
     const val WEBSOCKET_MAPPING_PREFIX = "/ws/{version}"
